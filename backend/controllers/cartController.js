d404fc28a9d73519ad61a08da9ad6edb6fef5064
John Paul Dellova <dellova36@gmail.com>
2025-02-23 22:20:11 +0800

    increments the quantity by adding the original price of the item being incremented

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -111,9 +111,10 @@ module.exports = {
                 const existingItemIndex = cart.cartItems.findIndex(item => item.foodId._id.toString() === foodId);
 
                 if (existingItemIndex > -1) {
-                    cart.cartItems[existingItemIndex].quantity += 1;
-                    cart.cartItems[existingItemIndex].totalPrice += cart.cartItems[existingItemIndex].totalPrice / (cart.cartItems[existingItemIndex].quantity - 1);
-                    cart.totalAmount += cart.cartItems[existingItemIndex].totalPrice / cart.cartItems[existingItemIndex].quantity;
+                    const item = cart.cartItems[existingItemIndex];
+                    item.quantity += 1;
+                    item.totalPrice += item.foodId.price;
+                    cart.totalAmount += item.foodId.price;
 
                     await cart.save();
                     return res.status(200).json({ status: true, message: 'Quantity incremented successfully' });
