3d62abbaa3dc025139a86ad6b63156c99d010bba
John Paul DELLOVA <John.Dellova@plus.local>
2025-03-17 11:50:58 +0800

    First Commit

--- backend/routes/order.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const orderController = require('../controllers/orderController');
-const { verifyAndAuthorization } = require('../middleware/verifyToken');
-
-router.post('/check-out', verifyAndAuthorization, orderController.checkoutOrder);
-router.get('/', verifyAndAuthorization, orderController.getUserOrders);
-router.post('/cancel', verifyAndAuthorization, orderController.cancelOrderByCustomer);
-
-module.exports = router;
---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
+
+<template>
+  <NavigationBar />
+</template>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
